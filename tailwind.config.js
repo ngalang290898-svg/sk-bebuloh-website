8fdaf6299684e624478c4894a2e6d947c7c1aa4f
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-28 12:20:07 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pastelPink: "#FFD1DC",
+        pastelBlue: "#C7E9FB",
+        pastelYellow: "#FFF5BA",
+        pastelGreen: "#C1E1C1",
+        pastelPurple: "#E3D4F3",
+      },
+    },
+  },
+  plugins: [],
+};
