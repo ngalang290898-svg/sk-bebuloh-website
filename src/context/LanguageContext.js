965ac3e21b7b4c16db250f8be9dccd6490234085
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-28 16:13:02 +0800

    Create LanguageContext.js

---
+++ src/context/LanguageContext.js
@@ -0,0 +1,19 @@
+// src/context/LanguageContext.js
+import { createContext, useContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+export function LanguageProvider({ children }) {
+  const [lang, setLang] = useState("en"); // default en
+  return (
+    <LanguageContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const ctx = useContext(LanguageContext);
+  if (!ctx) throw new Error("useLanguage must be used within LanguageProvider");
+  return ctx;
+}
