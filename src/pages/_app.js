3ba73d8ed58743f6224a2d1896d5a1292c3d38f6
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-28 16:14:00 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
 // src/pages/_app.js
 import "../styles/globals.css";
 import Head from "next/head";
+import { LanguageProvider } from "../context/LanguageContext";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <LanguageProvider>
       <Head>
         <title>SK Bebuloh</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Official site - SK Bebuloh" />
       </Head>
+      <Navbar />
       <Component {...pageProps} />
-    </>
+      <Footer />
+    </LanguageProvider>
   );
 }
