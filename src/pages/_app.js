d698c49a70ae1696ddabc997f35ee2a3f540ba0c
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-28 16:20:13 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,14 @@ import { LanguageProvider } from "../context/LanguageContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+<Head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</Head>
+
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
