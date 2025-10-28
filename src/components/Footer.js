e09c666c04e383dac06144ab81a83767e148e1b5
ngalang290898-svg <ngalang290898@gmail.com>
2025-10-28 12:11:35 +0800

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-500 py-4 bg-gray-100 mt-8">
+      <p>© {new Date().getFullYear()} SK Bebuloh Labuan. All Rights Reserved.</p>
+    </footer>
+  );
+}
